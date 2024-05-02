2b3b6b7e1fbf7163e8b782514791054bf4b86470
Mazi <martin.mazac1@gmail.com>
2024-05-02 21:13:35 +0200

    Adjust favicon and grid styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,9 @@ import Grid from "./Grid";
 function App() {
   return (
     <>
+      <h1>
+        Hex<sup>2</sup>
+      </h1>
       <Grid></Grid>
     </>
   );
--- frontend/src/Grid.tsx
+++ frontend/src/Grid.tsx
@@ -1,22 +1,31 @@
 import { GridGenerator, HexGrid, Layout, Hexagon } from "react-hexgrid";
+import "./Grid.css";
 
 function Grid() {
   const hexagonSize = { x: 3, y: 3 };
   const hexagons = GridGenerator.hexagon(10);
   return (
     <>
-      <HexGrid>
-        <Layout
-          size={hexagonSize}
-          flat={true}
-          spacing={1.05}
-          origin={{ x: 0, y: 0 }}
-        >
-          {hexagons.map((hex, index) => (
-            <Hexagon key={index} q={hex.q} r={hex.r} s={hex.s} />
-          ))}
-        </Layout>
-      </HexGrid>
+      <div className="grid-wrapper">
+        <HexGrid>
+          <Layout
+            size={hexagonSize}
+            flat={false}
+            spacing={1.05}
+            origin={{ x: 0, y: 0 }}
+          >
+            {hexagons.map((hex, index) => (
+              <Hexagon
+                className="hexi"
+                key={index}
+                q={hex.q}
+                r={hex.r}
+                s={hex.s}
+              />
+            ))}
+          </Layout>
+        </HexGrid>
+      </div>
     </>
   );
 }
